36b576c85dfc7067413b0ab20105ebab5a03261b
Chandler Dearden <chandlerjdearden@gmail.com>
2022-12-16 15:47:01 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Square from './Square';
 
 function App() {
+  const [squares, setSquares] = useState(['','','','','','','','',''])
+  const [player, setPlayer] = useState(true)
+  const propVariable = "Hello There"
+
+  const handleClick = () => {
+    setSquares(['','','','','','','','',''])
+    setPlayer(true)
+  }
+
+  const calculateWinner = (array) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ];
+    for (let i = 0; i<lines.length; i++) {
+      const [a,b,c] = lines[i];
+      if(
+        array[a] &&
+        array[a] === array[b] &&
+        array[a] === array[c]
+      ) {
+        return `${array[a]} Won!`
+      }
+    }
+    return "Who will Win?";
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        {squares.map((val, index) => {
+        return (
+          <Square
+          squareValue={val}
+          index={index}
+          squares ={squares}
+          setSquares = {setSquares}
+          player ={player}
+          setPlayer = {setPlayer} 
+          propsVar={propVariable}/>
+        );
+      })} </div>
+      <button onClick={handleClick}>Reset</button>
+      <br />
+      <span>
+        {calculateWinner(squares)}
+      </span>
     </div>
   );
 }
